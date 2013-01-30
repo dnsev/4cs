80726f1b66a2bb3dee91b090b1dc8ac981aef6af
dnsev <>
2013-01-29 22:27:17 -0800

    changelog-debug
    Not working...

--- script.js
+++ script.js
@@ -167,12 +167,15 @@ var page_browser = new PageBrowser();
 function get_change_log() {
 	var log_url = "https://raw.github.com/dnsev/4cs/master/web/changelog.txt";
 	$.ajax({
+		type: "GET",
 		url: log_url,
 		dataType: "text",
+		crossDomain: true,
 		success: function (data, status, jqXHR) {
 			parse_change_log(data);
 		},
 		error: function (jqXHR, status, error) {
+			alert(status+","+error+";");
 			$("#change_log").css("display", "");
 		}
 	});
