9b10f22a4fef2c6dce278dbb6eb10218a81db027
dnsev <dnsev0@gmail.com>
2013-05-20 14:56:19 -0400

    4.6.3
    Nothing changed but it thinks it did

--- web/4cs.full.user.js
+++ web/4cs.full.user.js
@@ -7,10 +7,9 @@
 // @grant       GM_info
 // @grant       GM_getValue
 // @grant       GM_setValue
-// @include     http://boards.4chan.org/*
-// @include     https://boards.4chan.org/*
-// @include     http://archive.foolz.us/*
-// @include     https://archive.foolz.us/*
+// @include     *://boards.4chan.org/*
+// @include     *://archive.foolz.us/*
+// @include     *://loveisover.me/*
 // @include     http://dnsev.github.io/4cs/*
 // @icon        data:image/gif;base64,R0lGODlhEAAQAKECAAAAAGbMM////////yH5BAEKAAIALAAAAAAQABAAAAIllI+pB70KQgAPNUmroDHX7Gie95AkpCUn1ISlhKVR/MEre6dLAQA7
 // @updateURL   https://raw.github.com/dnsev/4cs/master/web/4cs.full.meta.js
